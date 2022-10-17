69fa03099c4d89c7a2c39e6c5b54e855ad785693
Cynthia Munyasya <munyasyacynthia@gmail.com>
2022-10-17 12:04:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (pass)
+ */
+int main(void)
+{
+	char bas;
+
+	for (bas = 'a'; bas <= 'z'; bas++)
+	{
+		putchar(bas);
+	}
+	putchar('\n');
+	return (0);
+}
