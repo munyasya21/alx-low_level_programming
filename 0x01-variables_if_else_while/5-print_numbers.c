c42b6961ce98da689ae1bc4707634d25ee48170a
Cynthia Munyasya <munyasyacynthia@gmail.com>
2022-10-17 12:20:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: Cynthia
+ * Return: base 0 (pass)
+ */
+int main(void)
+{
+	int big;
+
+	for (big = 0; big < 10; big++)
+	{
+		printf("%d", big);
+	}
+	printf("\n");
+	return (0);
+}
