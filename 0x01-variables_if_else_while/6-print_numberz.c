16abd5cedc28b2dfce2db2dae213b7c5f3f8a4c7
Cynthia Munyasya <munyasyacynthia@gmail.com>
2022-10-17 12:23:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: Cynthia
+ * Return: Always 0(pass)
+ */
+int main(void)
+{
+	int base;
+
+	for (base = 48; base < 58; base++)
+		putchar(base);
+	putchar('\n');
+	return (0);
+}
