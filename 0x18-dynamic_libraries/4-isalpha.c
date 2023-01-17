a69bb1e1135d930a1802ff527fd05e567c9378f9
Cynthia Munyasya <munyasyacynthia@gmail.com>
2023-01-17 10:20:41 -0800

    My Tasks

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ * @c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                  0,1           All
+
